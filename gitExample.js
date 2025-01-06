90bbfbd0703372a1e7906cdcaa7481f38e095cb9
parimala1487 <parikolaganti@gmail.com>
2025-01-06 06:05:33 -0500

    new line added for branch2

--- gitExample.js
+++ gitExample.js
@@ -5,7 +5,7 @@ export default class GitExample extends LightningElement {
     handleSave()
     {
         console.log('This is first line of handle method in child branch1');
-        console.log('child method has been modified by me');
+        console.log('Added code by child branch2');
 
     }
   
