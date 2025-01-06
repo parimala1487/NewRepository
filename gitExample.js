dfd86697af6f5824ea5e08821095abcceaf4c490
parimala1487 <parikolaganti@gmail.com>
2025-01-06 06:17:53 -0500

    added variable

--- gitExample.js
+++ gitExample.js
@@ -2,10 +2,11 @@ import { LightningElement } from 'lwc';
 
 export default class GitExample extends LightningElement {
 
+    dataRecorded;
     handleSave()
     {
         console.log('This is first line of handle method in child branch1');
-        console.log('child method has been modified by me');
+        console.log('child method has been modified by me',this.dataRecorded);
 
     }
   
