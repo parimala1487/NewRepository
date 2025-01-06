5c8eb0e01e5a7075078718706fecc0f02cd0c00f
parimala1487 <parikolaganti@gmail.com>
2025-01-06 06:00:53 -0500

    added one more line

--- gitExample.js
+++ gitExample.js
@@ -5,6 +5,7 @@ export default class GitExample extends LightningElement {
     handleSave()
     {
         console.log('This is first line of handle method in child branch1');
+        console.log('child method has been modified by me');
 
     }
   
